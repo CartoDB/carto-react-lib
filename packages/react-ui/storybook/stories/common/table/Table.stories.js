79a1a241d2da0ee8ef88159e8dc8b9b02d494bc5
builtbyedgar <eledgaar@gmail.com>
2021-06-21 10:24:38 +0200

    sorting

--- packages/react-ui/storybook/stories/common/table/Table.stories.js
+++ packages/react-ui/storybook/stories/common/table/Table.stories.js
@@ -49,3 +49,21 @@ const Template = ({ ...args }) => {
 };
 
 export const Default = Template.bind({});
+
+// NOTE: only for table body rows
+// function getComparator(order, orderBy) {
+//   return order === 'desc'
+//     ? (a, b) => descendingComparator(a, b, orderBy)
+//     : (a, b) => -descendingComparator(a, b, orderBy);
+// }
+
+// NOTE: only for table body rows
+// function stableSort(array, comparator) {
+//   const stabilizedThis = array.map((el, index) => [el, index]);
+//   stabilizedThis.sort((a, b) => {
+//     const order = comparator(a[0], b[0]);
+//     if (order !== 0) return order;
+//     return a[1] - b[1];
+//   });
+//   return stabilizedThis.map((el) => el[0]);
+// }
--- packages/react-ui/storybook/stories/common/table/TableHead.stories.js
+++ packages/react-ui/storybook/stories/common/table/TableHead.stories.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Table,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
+  TableSortLabel,
   makeStyles
 } from '@material-ui/core';
 
@@ -18,11 +19,22 @@ export default options;
 const useStyles = makeStyles((theme) => ({
   tableHead: {
     backgroundColor: theme.palette.common.white,
-    '& .MuiTableCell-head': {
+    '& .MuiTableCell-head, & .MuiTableCell-head span': {
       border: 'none',
       ...theme.typography.subtitle2,
       color: theme.palette.primary.main
     }
+  },
+  visuallyHidden: {
+    position: 'absolute',
+    top: 20,
+    width: 1,
+    height: 1,
+    margin: -1,
+    padding: 0,
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    overflow: 'hidden'
   }
 }));
 
@@ -59,4 +71,62 @@ const Template = ({ ...args }) => {
   );
 };
 
+const TemplateSorting = ({ ...args }) => {
+  const classes = useStyles();
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('address');
+
+  const headCells = [
+    { id: 'address', label: 'Address' },
+    { id: 'city', label: 'City' },
+    { id: 'state', label: 'State / Province' },
+    { id: 'zip', label: 'Post Code' },
+    { id: 'country', label: 'Country' },
+    { id: 'geocode', label: 'Geocode', align: 'right' }
+  ];
+
+  function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  }
+
+  const handleRequestSort = (property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead className={classes.tableHead}>
+          <TableRow>
+            {headCells.map((headCell) => (
+              <TableCell
+                key={headCell.id}
+                align={headCell.align || 'left'}
+                padding={headCell.disablePadding ? 'none' : 'default'}
+              >
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : 'asc'}
+                  onClick={() => handleRequestSort(headCell.id)}
+                >
+                  {headCell.label}
+                </TableSortLabel>
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+      </Table>
+    </TableContainer>
+  );
+};
+
 export const Default = Template.bind({});
+export const Sorting = TemplateSorting.bind({});
